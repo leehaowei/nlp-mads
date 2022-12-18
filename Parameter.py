2f6aab99adbc337d39303a57b32c4bedae458b83
Haowei Lee <haoweilee.tpe@gmail.com>
2022-12-18 14:53:43 +0100

    feat: add Bert param

--- Parameter.py
+++ Parameter.py
@@ -35,4 +35,23 @@ class M4Param:
     max_features: int
     max_len: int
     verbose: 1
-    max_expoch: int
+    max_epoch: int
+    learning_rate: float
+    n_cycles: int
+    cycle_len: int
+    cycle_mult: int
+
+
+@dataclass
+class BertParam:
+    text_col: str
+    label_col: str
+    max_features: int
+    max_len: int
+    verbose: int
+    pre_trained: str
+    batch: int
+    max_epoch: int
+    n_cycles: int
+    cycle_len: int
+    cycle_mult: int
